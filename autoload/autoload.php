eeb94fb76e4f741e4adbb6ebbb8cf14c98d4faec
valer1on <val6785@mail.ru>
2016-12-09 23:06:12 +0200

    rename framework folder

--- autoload/autoload.php
+++ autoload/autoload.php
@@ -3,14 +3,23 @@
 
 require __DIR__ . '/Autoloader.php';
 
-$autoloader = new Autoloader();
+$loader = new Autoloader();
 
-$autoloader->add('Helpers', __DIR__ . '/../framework/Helpers');
-$autoloader->add('Http', __DIR__ . '/../framework/Http');
-$autoloader->add('Support', __DIR__ . '/../framework/Support');
-$autoloader->add('Traits', __DIR__ . '/../framework/Traits');
-$autoloader->add('App', __DIR__ . '/../framework/App');
-$autoloader->add('Database', __DIR__ . '/../framework/Database');
-$autoloader->add('Psr', __DIR__ . '/../framework/Psr');
+      // register the autoloader
+      $loader->register();
 
-$autoloader->register();
+      // register the base directories for the namespace prefix
+      $loader->addNamespace('App', __DIR__ . '/../Framework/App');
+//      $loader->addNamespace('Foo\Bar', '/path/to/packages/foo-bar/tests');
+
+//$autoloader = new Autoloader();
+//
+//$autoloader->add('Helpers', __DIR__ . '/../Framework/Helpers');
+//$autoloader->add('Http', __DIR__ . '/../Framework/Http');
+//$autoloader->add('Support', __DIR__ . '/../Framework/Support');
+//$autoloader->add('Traits', __DIR__ . '/../Framework/Traits');
+//$autoloader->add('App', __DIR__ . '/../Framework/App');
+//$autoloader->add('Database', __DIR__ . '/../Framework/Database');
+//$autoloader->add('Psr', __DIR__ . '/../Framework/Psr');
+//
+//$autoloader->register();
