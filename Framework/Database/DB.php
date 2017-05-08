d6075e2acd175067a0da3bb81abf82bb969489e6
valeriy <valeriy.nezhuriov@gmail.com>
2017-05-08 21:12:29 +0300

    remove ORM

--- Framework/Database/DB.php
+++ Framework/Database/DB.php
@@ -25,46 +25,28 @@ class DB {
 
     public static function pushConnection($nameConnection, $driver, $host, $port, $database, $user, $write = true, $read = true) {
 
-        $connection = new Connection($nameConnection, $driver, $host, $port, $database, $user, $write, $read);
+        $connection = new Connection(
+            $nameConnection,
+            $driver,
+            $host,
+            $port,
+            $database,
+            $user,
+            $write,
+            $read
+        );
 
         static::$connectionPool[$nameConnection] = $connection;
 
         static::setCurrentConnection($nameConnection);
-
-//        static::updateConnection();
-    }
-
-    protected static function updateConnection() {
-
-        $connection = static::getCurrentConnection();
-
-        ORM::configure( $connection->driver() . ":host={$connection->host()};dbname={$connection->database()}", null, $connection->name());
-        ORM::configure('username', $connection->user(), $connection->name());
-        ORM::configure('password', $connection->password(), $connection->name());
     }
 
     public static function setCurrentConnection($name) {
         static::$currentConnection = static::$connectionPool[$name];
-
-        static::updateConnection();
     }
 
     public static function getCurrentConnection() {
         return static::$currentConnection;
-    }
-
-
-    public static function query(string $query) {
-
-        $pdo = ORM::get_db(static::$currentConnection->name());
-
-        $result = $pdo->query($query);
-
-        return $result;
-    }
-
-    public static function table($tableName){
-        return ORM::for_table($tableName, static::$currentConnection->name());
     }
 
 
